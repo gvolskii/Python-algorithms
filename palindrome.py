d69e44d121578f8d95e57f52f64fa166683b2149
gvolskii <47405015+gvolskii@users.noreply.github.com>
2021-02-24 15:56:48 +0300

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,10 @@
+def palindrome(string):
+    symbols = Deque()
+    for symbol in string.lower():
+        if symbol != ' ':
+            symbols.addTail(symbol)
+    while symbols.size():
+        head, tail = symbols.removeFront(), symbols.removeTail()
+        if head != tail and head is not None and tail is not None:
+            return False
+    return True
