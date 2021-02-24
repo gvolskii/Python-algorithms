10e6f95d8a89446494f03c845139afd375a64083
gvolskii <47405015+gvolskii@users.noreply.github.com>
2021-02-24 13:13:10 +0300

    Create SimpleDeque.py

---
+++ SimpleDeque.py
@@ -0,0 +1,23 @@
+class Deque:
+    
+    def __init__(self):
+        self.deque = []
+
+    def addFront(self, item):
+        self.deque.insert(0, item)
+        
+    def addTail(self, item):
+        self.deque.append(item)
+
+    def removeFront(self):
+        if self.size():
+            return self.deque.pop(0)
+        return None
+
+    def removeTail(self):
+        if self.size():
+            return self.deque.pop()
+        return None
+
+    def size(self):
+        return len(self.deque)
